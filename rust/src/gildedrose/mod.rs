a1dc888ef950ee80387d0470e228fbf12b00e5ef
Chris McKenzie <crmckenzie@gmail.com>
2019-05-22 18:55:26 -0400

    Add a short-circuit for Sulfuras and remove the related `if` statements.

--- rust/src/gildedrose/mod.rs
+++ rust/src/gildedrose/mod.rs
@@ -24,11 +24,13 @@ impl GildedRose {
 
     pub fn update_quality(&mut self) {
         for item in &mut self.items {
+            if item.name == "Sulfuras, Hand of Ragnaros" {
+                break;
+            }
+
             if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
                 if item.quality > 0 {
-                    if item.name != "Sulfuras, Hand of Ragnaros" {
                         item.quality = item.quality - 1;
-                    }
                 }
             } else {
                 if item.quality < 50
@@ -51,17 +53,13 @@ impl GildedRose {
                 }
             }
 
-            if item.name != "Sulfuras, Hand of Ragnaros" {
-                item.sell_in = item.sell_in - 1;
-            }
+            item.sell_in = item.sell_in - 1;
 
             if item.sell_in < 0 {
                 if item.name != "Aged Brie" {
                     if item.name != "Backstage passes to a TAFKAL80ETC concert" {
                         if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality = item.quality - 1;
-                            }
+                            item.quality = item.quality - 1;
                         }
                     } else {
                         item.quality = item.quality - item.quality;
