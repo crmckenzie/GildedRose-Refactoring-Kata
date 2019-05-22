2cbe17c5d7ebf91b2cff647c71b0b2bf2767416a
Chris McKenzie <crmckenzie@gmail.com>
2019-05-22 18:33:23 -0400

    quality cannot be negative

--- rust/src/gildedrose/test.rs
+++ rust/src/gildedrose/test.rs
@@ -21,9 +21,19 @@ pub fn standard_item() {
 
 #[test]
 pub fn after_sell_in_has_passed_degrade_quality_faster() {
-    let result = update_quality("foo", 00, 20);
+    let result = update_quality("foo", 0, 20);
 
     assert_eq!("foo", result.name);
     assert_eq!(-1, result.sell_in);
     assert_eq!(18, result.quality);
+}
+
+
+#[test]
+pub fn quality_cannot_be_negative() {
+    let result = update_quality("foo", 0, 0);
+
+    assert_eq!("foo", result.name);
+    assert_eq!(-1, result.sell_in);
+    assert_eq!(0, result.quality);
 }
