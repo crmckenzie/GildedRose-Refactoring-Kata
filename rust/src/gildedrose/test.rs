83ef447b7dca5af6a528a5a85507a54729588b2f
Chris McKenzie <crmckenzie@gmail.com>
2019-05-22 18:31:59 -0400

    Add ReadMe add stale item test

--- rust/src/gildedrose/test.rs
+++ rust/src/gildedrose/test.rs
@@ -18,3 +18,12 @@ pub fn standard_item() {
     assert_eq!(9, result.sell_in);
     assert_eq!(19, result.quality);
 }
+
+#[test]
+pub fn after_sell_in_has_passed_degrade_quality_faster() {
+    let result = update_quality("foo", 00, 20);
+
+    assert_eq!("foo", result.name);
+    assert_eq!(-1, result.sell_in);
+    assert_eq!(18, result.quality);
+}
