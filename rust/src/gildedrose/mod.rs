653920c3b63d57dc30e32d3d01f1dbf05e75d043
Chris McKenzie <crmckenzie@gmail.com>
2019-05-22 19:05:20 -0400

    Implemented backstage passes and extracted the related conditionals.

--- rust/src/gildedrose/mod.rs
+++ rust/src/gildedrose/mod.rs
@@ -36,29 +36,23 @@ impl GildedRose {
                 break;
             }
 
-            if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                if item.quality > 0 {
-                    item.quality = item.quality - 1;
-                }
-            } else {
-                if item.quality < 50
-                {
-                    item.quality = item.quality + 1;
+            if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                item.sell_in -= 1;
+                item.quality += if item.sell_in < 0 {
+                    -1 * item.quality
+                } else if item.sell_in < 5 {
+                    3
+                } else if item.sell_in < 10 {
+                    2
+                } else {
+                    1
+                };
+                if item.quality > 50 { item.quality = 50};
+                break;
+            }
 
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sell_in < 11 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if item.sell_in < 6 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+            if item.quality > 0 {
+                item.quality = item.quality - 1;
             }
 
             item.sell_in = item.sell_in - 1;
@@ -68,10 +62,8 @@ impl GildedRose {
                     if item.quality > 0 {
                         item.quality = item.quality - 1;
                     }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-        }
+                } 
+            }
         }
     }
 }
