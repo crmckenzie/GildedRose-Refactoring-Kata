83b5952a4a2dc7d3ddd2c47a709564ef0b9c56d3
Chris McKenzie <crmckenzie@gmail.com>
2019-05-22 18:37:12 -0400

    Quality cannot exceed 50.

--- rust/src/gildedrose/test.rs
+++ rust/src/gildedrose/test.rs
@@ -56,3 +56,12 @@ pub fn aged_brie_increases_in_quality_faster_after_sell_by() {
     assert_eq!(-1, result.sell_in);
     assert_eq!(22, result.quality);
 }
+
+
+#[test]
+pub fn quality_cannot_exceed_50() {
+    let result = update_quality("Aged Brie", 0, 50);
+
+    assert_eq!(-1, result.sell_in);
+    assert_eq!(50, result.quality);
+}
